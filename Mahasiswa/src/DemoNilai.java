354e1781c2993364e34bdde9c7b3baf61c030c50
Bungakhoeriah <114651565+Bungakhoeriah@users.noreply.github.com>
2023-10-11 15:42:23 +0700

    Add files via upload

---
+++ Mahasiswa/src/DemoNilai.java
@@ -0,0 +1,18 @@
+package DemoNilai;
+
+public class DemoNilai {
+
+    public static void main(String[] args) {
+     Nilai nilaiAkhir = new Nilai();
+        
+        nilaiAkhir.nim = "2210631170113";
+        nilaiAkhir.nama = "Bunga Khoeriah Utami";
+        nilaiAkhir.absen = 100;
+        nilaiAkhir.tugas = 95;
+        nilaiAkhir.uts = 80;
+        nilaiAkhir.uas = 90;
+        
+        nilaiAkhir.CetakNilai();
+    }
+
+}
