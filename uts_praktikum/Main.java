560465dd7709ad03f9d3af106172428abf5970d3
Bungakhoeriah <114651565+Bungakhoeriah@users.noreply.github.com>
2023-11-12 20:24:23 +0700

    Update Main.java

--- uts_praktikum/Main.java
+++ uts_praktikum/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Soal1;
+package com.mycompany.uts_praktikum;
 import java.util.ArrayList;
 import java.util.List;
 /**
