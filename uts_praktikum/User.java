8d065cc78979a015e1ef461ae6930908ae956bf4
Bungakhoeriah <114651565+Bungakhoeriah@users.noreply.github.com>
2023-11-12 20:25:06 +0700

    Update User.java

--- uts_praktikum/User.java
+++ uts_praktikum/User.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Soal1;
+package com.mycompany.uts_praktikum;
 import java.util.ArrayList;
 import java.util.List;
 /**
  *
- * @author lenovo
+ * @author User
  */
 class User {
     private String username;
